6ce4b169368a5b5afdd4d56f38d059acbd27e5d7
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-06-11 20:55:45 +0900

    Initial Commit

---
+++ showHide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  showHide
+//
+//  Created by HEE TAE YANG on 2020/06/11.
+//  Copyright © 2020 yht. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
