a8a2c165276aa6504ead87bdfb952167d6b5a0df
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-06-11 21:50:45 +0900

    new

--- showHide/ContentView.swift
+++ showHide/ContentView.swift
@@ -9,13 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var truefalse = false
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {
+            if self.truefalse {
+                Text("메디콜 공중전화1")
+            } else {
+                Text("메디콜 공중전화1").hidden()
+            }
+            
+            if self.truefalse {
+                Text("메디콜 공중전화2").hidden()
+            } else {
+                Text("메디콜 공중전화2")
+            }
+            
+            ShowHidden(truefalse: $truefalse)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ShowHidden: View {
+    @Binding var truefalse: Bool
+    
+    var body: some View {
+        Button(action: {
+            self.truefalse = !self.truefalse
+        }){
+            Text("버튼")
+        }
     }
 }
